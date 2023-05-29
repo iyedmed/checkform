69f6a8b01069391eeb550e3ed5622b2287390fa9
iyedmed <iyedmarzougui113@gmail.com>
2023-05-29 15:49:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './components/Profile';
+import  Navbarrr from './components/Nabarrr'
+
+function App() {
+  return (
+    <div>
+      <Navbarrr/>
+   <Profile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nabarrr.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nabarrr() {
+  return (
+    <div className="nav">
+       <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+       </ul>
+    </div>
+  )
+}
+
+export default Nabarrr
